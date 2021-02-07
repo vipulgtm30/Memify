74998f60e0b76af4bd07fa2aa19a2dbc045cb320
vipulgtm30 <vipul.gtm30@gmail.com>
2021-02-07 13:53:25 +0530

    Splash Screen added

---
+++ app/src/main/java/com/example/sharememes/SplashScreen.kt
@@ -0,0 +1,34 @@
+package com.example.sharememes
+
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.os.HandlerCompat.postDelayed
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val textview = findViewById<TextView>(R.id.textView)
+
+        val typeface = ResourcesCompat.getFont(
+                this,
+                R.font.alexandra
+        )
+        textview.setTypeface(typeface)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) //this 3 millisecond time
+
+    }
+}
